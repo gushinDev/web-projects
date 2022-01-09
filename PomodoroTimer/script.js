d70dba4182cf67e3eb6caa6028f2243b11e710b9
gushinDev <gushindev@gmail.com>
2022-01-09 14:52:55 +0300

    add Pomodoro timer

---
+++ PomodoroTimer/script.js
@@ -0,0 +1,37 @@
+
+let progressCircle = document.querySelector(".progress");
+let radius = progressCircle.r.baseVal.value;
+//circumference of a circle = 2πr;
+let circumference = radius * 2 * Math.PI;
+progressCircle.style.strokeDasharray = circumference;
+
+//0 to 100
+
+
+let i = 0; 
+let seconds = 25 * 60;
+myLoop();
+
+function myLoop() {
+  setTimeout(function() {
+    
+    progressCircle.style.strokeDashoffset = circumference - (i / seconds) * circumference;
+    i++;
+    if(i === seconds + 1) {
+      i = 0;
+      seconds = 3;
+    }
+    if(i < seconds + 1) {
+      console.log(i, seconds)
+      myLoop();
+    }
+    
+    
+  }, 1000)
+}
+
+
+function setProgress() {
+  progressCircle.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+}
+
