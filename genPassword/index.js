e2079929166705ad0d8915b66be1a16d121e8c96
gushinDev <gushindev@gmail.com>
2022-03-20 23:14:33 +0300

    add some features

--- genPassword/index.js
+++ genPassword/index.js
@@ -1,15 +1,17 @@
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z'];
-
+const symbols = '` ~ ! @ # $ % ^ & * ( ) _ - + = { } [ ] \' | : ; " < > , . ? / '.split(' ');
 const numbers = [0,1,2,3,4,5,6,7,8,9];
+const allSymbols = [...letters, ...symbols, ...numbers]
+console.log(allSymbols);
 
 let generatorText = document.querySelector('.generator__text')
 
-generatorText.textContent = generatePassword(10, letters);
+generatorText.textContent = generatePassword(10, allSymbols);
 
 function generatePassword(passwordLength) {
     let password = '';
     for (let i = 0; i < passwordLength; i++) {
-        password+=letters[Math.floor(Math.random()*letters.length)]
+        password+=allSymbols[Math.floor(Math.random()*allSymbols.length)]
     }
     return password;
 }
@@ -22,6 +24,7 @@ let numberOfLetters = document.querySelector('.settings__number-letters')
 range.addEventListener('input', (event)=>{
     numberOfLetters.value = event.target.value;
     generatorText.textContent = generatePassword(numberOfLetters.value);
+    setDefaultStateCopyButton();
 })
 
 
@@ -29,18 +32,28 @@ range.addEventListener('input', (event)=>{
 let copyBtn = document.querySelector('.btn__copy');
 
 copyBtn.addEventListener('click', (event)=>{
-        
+    copyPasswordToClipboard();
+    event.target.textContent = 'copied';
+    event.target.style.backgroundColor = 'rgb(166, 238, 202)';
+})
+
+function copyPasswordToClipboard() {
     let range = document.createRange();
     range.selectNode(document.querySelector(".generator__text"));
     window.getSelection().removeAllRanges();  
     window.getSelection().addRange(range); 
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
+}
 
-})
+function setDefaultStateCopyButton() {    
+    copyBtn.textContent = "Copy";
+    copyBtn.style.backgroundColor = null;
+}
 
 //Update button
 let updateButton = document.querySelector('.btn__update');
 updateButton.addEventListener('click', (event)=>{
     generatorText.textContent = generatePassword(document.querySelector('#numberLetters').value)
+    setDefaultStateCopyButton();
 })
