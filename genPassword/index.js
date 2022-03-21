9281bd9fc571d61ab8c5f5daf2722e0af4be4087
gushinDev <gushindev@gmail.com>
2022-03-21 13:05:45 +0300

    add upper case letters

--- genPassword/index.js
+++ genPassword/index.js
@@ -1,7 +1,8 @@
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseLetters = letters.map(item => item.toUpperCase());
 const symbols = '` ~ ! @ # $ % ^ & * ( ) _ - + = { } [ ] \' | : ; " < > , . ? / '.split(' ');
 const numbers = [0,1,2,3,4,5,6,7,8,9];
-const allSymbols = [...letters, ...symbols, ...numbers]
+const allSymbols = [...letters, ...symbols, ...numbers, ...upperCaseLetters]
 console.log(allSymbols);
 
 let generatorText = document.querySelector('.generator__text')
